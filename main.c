e218be0f14a54c3fb3d4ca6fe2ed7c6a99c9b16a
elias.ayeb@gmail.com <elias.ayeb@gmail.com>
2025-04-08 09:03:17 +0200

    test5

--- main.c
+++ main.c
@@ -6,17 +6,17 @@ int main()
     // The Line below must *NOT* be changed or test will fail!
     srand(1); 
     int counts[7]= {0};
-    int sum = 0;
+    int sum =0;
 
     for (int i =0; i<100; i++){
-        int roll = (rand() % 6) +1;
+        int roll = (rand() % 6) +1; // Generate a random number between 1 and 6
         counts[roll]++;
         sum+=roll;
     }
     for (int i = 1; i <=6; i++){
-        printf("%d\n", counts[i]); 
+        printf("%d\n", counts[i]) 
     }
-   printf("%d", sum);
+   printf("%d\n", sum);
    float average = (float)sum / 100;
    printf("%.1f", average);
 
